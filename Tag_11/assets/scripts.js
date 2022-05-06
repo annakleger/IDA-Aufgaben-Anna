b6e94a4974caee75aaf161c3adeb9ed596fb7207
annakleger <anna.kleger@stud.hslu.ch>
2022-05-06 09:47:31 +0200

    update

---
+++ Tag_11/assets/scripts.js
@@ -0,0 +1,21 @@
+let n = 0; /*schlüsselwort let und variablenname. zuweisung des werts von rechts nach links */
+function buttonPressed() {
+    n++;
+    document.getElementById("element").style.left = (n * 5) + "px";
+}
+
+// function neuesElement() {
+//     let div = document.createElement("div");
+//     div.classList.add('random');
+//     div.style.left = Math.random() * window.innerWidth + "px";
+//     div.style.top = Math.random() * window.innerHeight + "px";
+//     document.body.appendChild(div);
+// }
+
+function neuesElement() {
+    let div = document.createElement("div");
+    div.classList.add('random');
+    div.style.left = Math.random() * window.innerWidth + "px";
+    div.style.top = Math.random() * window.innerHeight + "px";
+    document.body.appendChild(div);
+}
---
+++ arrays/scripts/script.js
@@ -0,0 +1,21 @@
+console.log('Hello World');
+let number = 9
+let string = 'string'
+
+// arrays 
+
+console.log(number)
+
+let array = [3, 5, 67, 8, 90, 43, 12, 3, 4]
+
+// console.log(array)
+// console.log(array[8])
+
+for (let i = 0; i < 5; i += 1)
+
+{
+    console.log('for loop in position: ');
+    console.log(i);
+    console.log('show array in position')
+    console.log(array[i])
+}
